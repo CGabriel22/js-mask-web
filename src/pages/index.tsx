e0f245df22c4ed7d14e4af5827bb387555f72bc4
CGabriel22 <carlosgabrieldev@gmail.com>
2021-09-11 19:48:51 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import { NextPage } from 'next'
+
+const Home: NextPage = () => {
+
+  return (
+    <>
+      <Head>
+        <title>js mask</title>
+      </Head>
+
+    </>
+  )
+}
+export default Home
